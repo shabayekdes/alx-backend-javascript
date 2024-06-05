2c1b527dfb49827d32bf0b92f3add389f3a72fd7
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-06-05 22:11:10 +0300

    Update task 2

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,6 @@
 /**
  * Displays a message on the STDOUT.
- * 
- * This module exports a function that takes a message 
+ * This module exports a function that takes a message
  * as an argument and logs it to the console
  */
 module.exports = (message) => console.log(message);
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,22 @@
+// Write a message to the console, asking the user for their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Set up an event listener for the 'readable' event on the process.stdin stream
 process.stdin.on('readable', () => {
+  // Read the available data from the stream
   const chunk = process.stdin.read();
 
+  // Check if the chunk is not null or undefined
   if (chunk) {
+    // Write a response to the console, including the user's input
     process.stdout.write(`Your name is: ${chunk}`);
+    // Close the input stream to prevent further input
+    process.stdin.destroy();
   }
 });
 
+// Set up an event listener for the 'end' event on the process.stdin stream
 process.stdin.on('end', () => {
+  // Write a farewell message to the console
   process.stdout.write('This important software is now closing\n');
 });
